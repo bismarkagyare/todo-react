7d891da3188a297155f5debd15c72bfcb6d19c9e
bismarkagyare <bismarkagyare54@gmail.com>
2024-02-19 18:48:43 +0000

    import header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Layout from './layout/layout';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div>
-      <div>app</div>
+    <div className='bg-slate-300'>
+      <Header />
     </div>
   );
 };
