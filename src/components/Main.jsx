a0067eeb6d1b0720899dca7c714a993b45185b3e
bismarkagyare <bismarkagyare54@gmail.com>
2024-02-22 21:15:10 +0000

    implement main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div>
+      <div>Main content goes here</div>
+      <p>lorem100</p>
+    </div>
+  );
+};
+
+export default Main;
