b40a30782723c7ac0a98b333b92cc8169c4a9212
bismarkagyare <bismarkagyare54@gmail.com>
2024-02-22 21:15:53 +0000

    incoporate the following features: main & sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import Layout from './layout/layout';
+//import Layout from './layout/layout';
 import Header from './components/Header';
-
+import Sidebar from './components/Sidebar';
+import Main from './components/Main';
 const App = () => {
   return (
     <div className='bg-slate-300'>
       <Header />
+      <div className='grid grid-cols-4'>
+        <Sidebar />
+        <Main />
+      </div>
     </div>
   );
 };
