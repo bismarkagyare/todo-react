95909142cc6804351462fa7843d7fd4e9731fc9a
bismarkagyare <bismarkagyare54@gmail.com>
2024-02-22 21:15:25 +0000

    implement sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BiTask } from 'react-icons/bi';
+import { SlCalender } from 'react-icons/sl';
+import { IoCalendarNumberSharp } from 'react-icons/io5';
+import { FaStar } from 'react-icons/fa6';
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div>
+        <h2>Home</h2>
+        <article className='flex items-center gap-2 text-lg'>
+          <BiTask />
+          <p>All Tasks</p>
+        </article>
+        <article className='flex items-center gap-2 text-lg'>
+          <SlCalender />
+          <p>Today</p>
+        </article>
+        <article className='flex items-center gap-2 text-lg'>
+          <IoCalendarNumberSharp />
+          <p>Upcoming</p>
+        </article>
+        <article className='flex items-center gap-2 text-lg'>
+          <FaStar />
+          <p>Important</p>
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
