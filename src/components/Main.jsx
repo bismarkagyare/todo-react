d862aa55422d336901230b6448230304ba75235e
bismarkagyare <bismarkagyare54@gmail.com>
2024-02-25 20:35:10 +0000

    add dummy text for now

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,19 @@ const Main = () => {
   return (
     <div>
       <div>Main content goes here</div>
-      <p>lorem100</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore iusto,
+        omnis, temporibus enim voluptatum sed porro nostrum natus dolores eos
+        dolorem. Voluptate ut vel adipisci, similique, quis quidem excepturi
+        debitis sequi, dolorem eos nobis neque ea beatae. Sint voluptates
+        distinctio repellendus perferendis dolorum a rem sit nobis. Explicabo,
+        ipsam! Quaerat atque voluptates velit dolor quidem iure cum mollitia
+        excepturi repellat reiciendis. Ex sapiente, voluptate doloremque
+        inventore quos aut. Impedit perspiciatis, nostrum explicabo quam
+        eligendi omnis doloribus reiciendis iste fugiat itaque excepturi quidem
+        magnam, ad saepe doloremque placeat maiores. Eligendi eum totam
+        aspernatur facere quis sunt magnam sed provident quaerat alias?
+      </p>
     </div>
   );
 };
